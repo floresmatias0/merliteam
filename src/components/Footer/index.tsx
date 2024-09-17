c6e588ff7222aa97a077d33f2693353a15433dd8
tomymagno <tomasmagnone@gmail.com>
2024-09-17 18:22:57 -0300

    cambios footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,21 +1,22 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { useTranslations } from 'next-intl'
 
 const Footer = () => {
   const t = useTranslations("Index")
 
   return (
-    <footer className='flex flex-col justify-between px-8 md:px-14 md:flex-row mb-4 mt-10'>
-      <div className='w-full md:w-1/3'>
-        <div>
-          <Image src='/MerliTeamLogo.svg' alt='Merliteam' width={200} height={200} className='m-auto md:m-[initial] w-24 md:w-24'/>
+    <footer className='flex justify-between items-center w-full h-[139px] bg-gradient-to-br from-[#070009] to-[#45025D] px-[56px] py-[32px]'>
+      <div className='ml-auto w-full md:w-auto flex flex-col items-end'>
+        <div className='flex space-x-4 relative top-[-4px] mb-2'>
+          <Link href='https://www.instagram.com/merliteam.ok/' target='_blank' rel='noopener noreferrer'>
+            <Image src='/logo-instagram.png' alt='Instagram' width={24} height={24} />
+          </Link>
+          <Link href='https://www.linkedin.com/in/merli-team-7778492a2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target='_blank' rel='noopener noreferrer'>
+            <Image src='/logo-linkedin.png' alt='LinkedIn' width={24} height={24} />
+          </Link>
         </div>
-        <div>
-          {/* {FOOTER_CONTENT.contact.t1} */}
-        </div>
-      </div>
-      <div className='w-full md:w-1/3 flex items-start justify-start md:items-end md:justify-end mt-8 md:mt-0'>
-        <small>
+        <small className='whitespace-nowrap'>
           {t("footer.copyright")}
         </small>
       </div>
