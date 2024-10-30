75f0101543019c290e9db5826c0938aa53466bb9
nicolas <nicolas>
2024-10-30 20:35:18 -0300

    solucionado el error del build

--- src/app/api/actions/uploadImage/route.ts
+++ src/app/api/actions/uploadImage/route.ts
@@ -1,22 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { v2 as cloudinary } from "cloudinary";
+// src/app/actions/updateImage/route.js
+import { NextRequest, NextResponse } from 'next/server';
+import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.config(process.env.CLOUDINARY_URL ?? "");
+cloudinary.config(process.env.CLOUDINARY_URL ?? '');
 
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
+export async function POST(request: NextRequest) {
   try {
-    const file = req.body.file; // El archivo debe ser enviado en el cuerpo de la solicitud
+    // Obtener el cuerpo de la solicitud
+    const body = await request.json();
+    const file = body.file; // El archivo debe ser enviado en el cuerpo de la solicitud
+
+    // Subir la imagen a Cloudinary
     const result = await cloudinary.uploader.upload(file, {
-      folder: "uploads",
+      folder: 'uploads',
     });
-    return res.status(200).json({ url: result.secure_url });
+
+    // Devolver la URL de la imagen subida
+    return NextResponse.json({ url: result.secure_url }, { status: 200 });
   } catch (error) {
-    console.error("Error uploading image:", error);
-    return res.status(500).json({ error: "Failed to upload image" });
+    console.error('Error uploading image:', error);
+    return NextResponse.json({ error: 'Failed to upload image' }, { status: 500 });
   }
 }
--- src/components/botonBorrarrr/index.tsx
+++ src/components/botonBorrarrr/index.tsx
@@ -3,6 +3,6 @@
 import { main } from "@/utils/seed";
 export default function Botoooon(){
     return(
-        <button className='p-9 w-[200px] h-[200px] ' onClick={(async () => await main())}>CREAAAAAAAAAAAR</button>
+        <button className='p-9 w-[200px] h-[200px]      ' onClick={(async () => await main())}>CREAAAAAAAAAAAR</button>
     )
 }
